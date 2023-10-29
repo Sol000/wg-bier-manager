9599b7bb41b9bcba6b0ac3bbbed535955aa3277c
Tippler Manuel <mtippler@student.tugraz.at>
2023-10-29 21:20:29 +0100

    💾 database manipulation

--- run.sh
+++ run.sh
@@ -2,8 +2,15 @@
 
 gradle -p backend/ bootJar
 sudo docker compose build
+
 if [ "$1" == "--prod" ]; then
+  if [ "$2" == "--hard-reset" ]; then
+    sudo rm -rf /var/lib/docker/volumes/mongodb_data_container
+  fi
   sudo docker compose --profile prod up -d
 else
+  if [ "$2" == "--hard-reset" ]; then
+    sudo rm -rf /var/lib/docker/volumes/mongodb_data_container
+  fi
   sudo docker compose up
 fi
