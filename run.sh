46942d45b62f489662f11b6ab6fcfdccd16b988f
Samuel Fahrngruber-Biernbaum <samuel.fahrngruber-biernbaum@student.tugraz.at>
2023-11-19 19:57:28 +0100

    👨‍🚒 fire fighting

--- run.sh
+++ run.sh
@@ -12,5 +12,5 @@ else
   if [ "$2" == "--hard-reset" ]; then
     sudo rm -rf /var/lib/docker/volumes/wg-bier-manager_mongodb_data_container
   fi
-  sudo docker compose up
+  sudo docker compose --profile local up
 fi
