f69a8b6c008deaf094b2cad0af0a2354c4a9f1f8
Tippler Manuel <mtippler@student.tugraz.at>
2023-11-20 21:45:38 +0100

    🩹 hard reset fix

--- run.sh
+++ run.sh
@@ -5,11 +5,13 @@ sudo docker compose build
 
 if [ "$1" == "--prod" ]; then
   if [ "$2" == "--hard-reset" ]; then
+    sudo docker compose --profile prod down 
     sudo docker volume rm wg-bier-manager_mongodb_data_container
   fi
   sudo docker compose --profile prod up --force-recreate --build -d
 else
   if [ "$2" == "--hard-reset" ]; then
+    sudo docker compose --profile local down 
     sudo docker volume rm wg-bier-manager_mongodb_data_container
   fi
   sudo docker compose --profile local up 
