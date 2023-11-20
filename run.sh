6201d50b1433f8e4a7f7e7373e51ab7235844d59
Tippler Manuel <mtippler@student.tugraz.at>
2023-11-20 21:41:57 +0100

    🩹 rm -rf 'rm -rf'

--- run.sh
+++ run.sh
@@ -5,12 +5,12 @@ sudo docker compose build
 
 if [ "$1" == "--prod" ]; then
   if [ "$2" == "--hard-reset" ]; then
-    sudo rm -rf /var/lib/docker/volumes/wg-bier-manager_mongodb_data_container
+    sudo docker volume rm wg-bier-manager_mongodb_data_container
   fi
   sudo docker compose --profile prod up --force-recreate --build -d
 else
   if [ "$2" == "--hard-reset" ]; then
-    sudo rm -rf /var/lib/docker/volumes/wg-bier-manager_mongodb_data_container
+    sudo docker volume rm wg-bier-manager_mongodb_data_container
   fi
   sudo docker compose --profile local up 
 fi
